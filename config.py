c5f1b96276b6a2704dfcfebd855ca32351136dea
Niriktelbot <110273551+Niriktelbot@users.noreply.github.com>
2022-09-04 14:54:17 +0600

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
-token = "vk1.a.AHV3zftVRBU8lutelu8AXRkSClSfcWXCJ9-kJJ7Q5cNHsevpGzg2PpjfdGXG_lBE1MvfRazkODDmc5fqvHFpDMltigYwyF8_I4pTWlFVxIlA9sB2t_X8j99eDPzeuAhYnWwo31rvS7EVNC27s6azskd_19GPX63WzjPiDLKQ_lnRZJmAugb30raVWYMGH8tZ
+token = "vk1.a.AHV3zftVRBU8lutelu8AXRkSClSfcWXCJ9-kJJ7Q5cNHsevpGzg2PpjfdGXG_lBE1MvfRazkODDmc5fqvHFpDMltigYwyF8_I4pTWlFVxIlA9sB2t_X8j99eDPzeuAhYnWwo31rvS7EVNC27s6azskd_19GPX63WzjPiDLKQ_lnRZJmAugb30raVWYMGH8tZ"
 merchant_key = "wAg[B*;m1G0s;TTH,A!o5hzF[N3iWS*tD8yjr.20U]YMk.q*AN" # vk.com/coin
 id = 634288269
 group_id = 213096773
